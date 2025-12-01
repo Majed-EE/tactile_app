5902776faffc3cc5b2d73c7a3920a06100516851
majed-ee <syed.majed.ashraf@gmail.com>
2025-12-01 16:19:04 +0530

    Toy arm base code

---
+++ proto_mqtt/UE_toy_arm_msqt_subscriber.py
@@ -0,0 +1,49 @@
+import paho.mqtt.client as mqtt
+import time
+import json
+
+
+
+def simple_subscriber():
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    
+    def on_connect(client, userdata, flags, reason_code, properties):
+        if reason_code == 0:
+            print("Subscriber connected!")
+            client.subscribe("UE/to_toy_arm")
+        else:
+            print(f"Connection failed: {reason_code}")
+    
+    def on_message(client, userdata, msg):
+        try:
+            data = json.loads(msg.payload.decode())
+            print(f"Received: {data}")
+            
+            # ask sarthak to update this variable
+            job_done=False
+            if job_done:
+                frm_msg="ok"
+                client.publish("UE/from_toy_arm", frm_msg)
+
+        except:
+            print(f"Received raw: {msg.payload.decode()}")
+    
+    client.on_connect = on_connect
+    client.on_message = on_message
+    local_broker=True
+    broker_local_address="10.10.7.199"
+    broker_address = broker_local_address if local_broker else "test.mosquitto.org"
+    client.connect(broker_address, 1883, 60)
+    
+    # This will block and keep listening for messages
+    print("Listening for messages...")
+    t_run=30
+    for x in range(t_run):
+        print(f"Waiting... {x+1}/{t_run}")
+        client.loop_start()
+        time.sleep(1)
+    client.loop_stop()
+
+if __name__ == "__main__":
+    simple_subscriber()
+    print("Subscriber finished.")
